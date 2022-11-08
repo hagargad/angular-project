f9e2d1fe5592e8721555656d58091e8b9a7afbcd
hagargad <hagar-hosny-gad@hotmail.com>
2022-11-09 01:36:03 +0200

    admin updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
@@ -8,6 +10,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   title = 'Tickets Marche';
-
-
+  constructor(public router: Router){}
 }
