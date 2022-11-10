011bf642ab70224b924d19927305b7c5b9dea0e5
kareemAshraf98 <karimhafez231@gmail.com>
2022-11-10 13:44:06 +0200

    updated

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ProjectService } from '../services/project.service';
 
 @Component({
   selector: 'app-login',
@@ -8,9 +9,10 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(public user:ProjectService) { }
 
   ngOnInit(): void {
+    this.user.getUserInfo
   }
   Userloginvalidation = new FormGroup({
     email : new FormControl('',Validators.email),
--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,9 @@ import { Injectable } from '@angular/core';
 })
 export class ProjectService {
 
-  constructor() { }
+  constructor(private myUser:HttpClient) { }
+  private Base_URL = "https://fakestoreapi.com/users";
+  getUserInfo(){
+    return this.myUser.get(this.Base_URL);
+  }
 }
