88fb19222c4bb9eb71fed44c65eb5090e87e4b51
hagargad <hagar-hosny-gad@hotmail.com>
2022-11-11 21:16:24 +0200

    updated

--- src/app/admin/admin-about/admin-about.component.ts
+++ src/app/admin/admin-about/admin-about.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AboutService } from '../../about/about.service';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { title } from 'process';
 
 @Component({
   selector: 'app-admin-about',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminAboutComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private myservice:AboutService) { }
+  title:any;
+  data:any;
   ngOnInit(): void {
+     //fetching
+     let that = this;
+
+     this.myservice.getData().subscribe({
+       next(data:any){
+         that.title=title;
+         that.data=data;
+
+       }
+
+     })
   }
 
 }
