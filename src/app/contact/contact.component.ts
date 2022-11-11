87ca1bb656afe771f7233509859161ae5c64eb94
Omar_Mahran <omar.a.m.mahran@gmail.com>
2022-11-11 22:02:16 +0200

    Update profile and contact2

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -12,9 +12,39 @@ export class ContactComponent implements OnInit {
   
   ngOnInit(): void {
   }
+  
   myContactFormValidation=new FormGroup({
-    Fname:new FormControl("Amed",Validators.required),
-    Lname:new FormControl("Ëslma",[Validators.minLength(3),Validators.maxLength(10)])
-
+    Fname:new FormControl("",[Validators.minLength(3),Validators.maxLength(10),Validators.required]),
+    Lname:new FormControl("",[Validators.minLength(3),Validators.maxLength(10),Validators.required]),
+    Email:new FormControl("",[Validators.required,Validators.email]),
+    Mobile:new FormControl(0,[Validators.minLength(11),Validators.maxLength(11),Validators.required]),
+    Massage:new FormControl("",[Validators.maxLength(200),Validators.required])
   })
+  get nameValid(){
+    return this.myContactFormValidation.controls.Fname.value 
+  }
+
+  cslicc=true
+  clicc(){
+    this.cslicc = !this.cslicc
+  }
+
+
+  successMsg="Suceessful Completed Will be contact with you soon "
+  failedMssage="please fill success Data"
+  tmeSendMail(){
+  
+    let successMsg
+    if (this.myContactFormValidation.valid){
+        successMsg = this.successMsg;
+      }
+    else{
+      successMsg = this.failedMssage;
+    }
+      return successMsg
+}
+
+sendData(){
+  
+}
 }
